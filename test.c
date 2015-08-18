3b034a7b26e2c0c02a53a46361418281d44c746f
Casber <casbercc@users.noreply.github.com>
2015-08-18 22:34:44 +0800

    Add content

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+    int a = 0x12345678;
+    int *p = &a;
+
+    printf("Hello world!\n");
+
+    if (p == 0x12)
+
+    return 0;
+}
